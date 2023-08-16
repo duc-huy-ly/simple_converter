3bd5ebd8cc96ce602f77dd54077b793bf90af20d
Duc Huy LY <lyduchuy98@gmail.com>
2023-08-16 18:16:21 +0200

    updated version with function

--- stage1.sh
+++ stage1.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 echo "Enter a definition:"
-read -a user_input
+read -r -a user_input
 arr_length="${#user_input[@]}"
 definition="${user_input[0]}"
 constant="${user_input[1]}"
@@ -9,21 +10,28 @@ constant="${user_input[1]}"
 stringRegex='^[a-z]+_to_[a-z]+$'
 numberRegex='^-?[0-9]+(\.[0-9]+)?$'
 
-if [ "$arr_length" == "2" ] && [[ "$definition" =~ $stringRegex ]] && [[ "$constant" =~ $numberRegex ]]; then
-    echo "Enter a value to convert:"
-    read input
-
-    while true; do
-        if ! [[ "$input" =~ ^$numberRegex$ ]]; then
-            echo "Enter a float or integer value!"
-            read  input
-        else
-            break
-        fi
-    done
-
+calculate () {
     result=$(echo "scale=2; $constant * $input" | bc -l)
     printf "Result: %s\n" "$result"
+}
+
+input() {
+    echo "Enter a value to convert:"
+        read -r input
+
+        while true; do
+            if ! [[ "$input" =~ ^$numberRegex$ ]]; then
+                echo "Enter a float or integer value!"
+                read -r input
+            else
+                break
+            fi
+        done
+        calculate
+}
+
+if [ "$arr_length" == "2" ] && [[ "$definition" =~ $stringRegex ]] && [[ "$constant" =~ $numberRegex ]]; then
+    input
 else
     echo "The definition is incorrect!"
 fi
