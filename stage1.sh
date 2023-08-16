eb5ca870ed887e69d71893e0e4cf98af71d725ce
Duc Huy LY <lyduchuy98@gmail.com>
2023-08-16 17:22:19 +0200

    able to calculate

--- stage1.sh
+++ stage1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "Enter a definition:"
+echo "Enter a definition:"
 read -a user_input
 arr_length="${#user_input[@]}"
 definition="${user_input[0]}"
@@ -10,7 +10,20 @@ stringRegex='^[a-z]+_to_[a-z]+$'
 numberRegex='^-?[0-9]+(\.[0-9]+)?$'
 
 if [ "$arr_length" == "2" ] && [[ "$definition" =~ $stringRegex ]] && [[ "$constant" =~ $numberRegex ]]; then
-    echo "The definition is correct!"
+    echo "Enter a value to convert:"
+    read input
+
+    while true; do
+        if ! [[ "$input" =~ ^$numberRegex$ ]]; then
+            echo "Enter a float or integer value!"
+            read  input
+        else
+            break
+        fi
+    done
+
+    result=$(echo "scale=2; $constant * $input" | bc -l)
+    printf "Result: %s\n" "$result"
 else
     echo "The definition is incorrect!"
 fi
